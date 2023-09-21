288df6ef077f72e03d46b9c23c839a66f68ac948
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2023-09-21 11:31:08 +0300

    Persist current ticket id value between multiple connections

--- lab1.py
+++ lab1.py
@@ -4,70 +4,73 @@ from threading import Thread
 from random import sample
 from collections import Counter
 
-CURRENT_TICKET_ID = 'current_ticket_id'
+UPCOMING_TICKET_ID = 'upcoming_ticket_id'
 
 VALID_NUMBERS = list(range(1, 41))
+NUMBER_OF_WINNER_NUMBERS = 5
 
-def find_winner_tickets(winner_numbers: list, connection: Redis):
-    winner_ticket_ids = []
+def get_winner_tickets(connection: Redis):
+    winner_numbers = sample(VALID_NUMBERS, NUMBER_OF_WINNER_NUMBERS)
+    winner_numbers.sort()
+    print(f'Winner numbers: {winner_numbers}')
+
     with connection.pipeline() as pipe:
         for number in winner_numbers:
             pipe.watch(number)
         pipe.multi()
         for number in winner_numbers:
-            winner_ticket_ids += pipe.get(number)
+            if connection.llen(number) > 0:
+                pipe.lrange(number, 0, -1)
         try:
-            pipe.execute()
-            # Counts the number of matching numbers in each ticket
-            print(Counter(winner_ticket_ids))
+            results = pipe.execute()
+            winner_ticket_ids = [int(ticket_id) for sublist in results for ticket_id in sublist]
+            print(f'Matching numbers of each winning ticket: {dict(Counter(winner_ticket_ids))}')
         except WatchError:
-            pipe.discard()
-            print('Another ticket is being updated at the moment. Try drawing again.')
-            return None
+            print('An error occurred while getting winner tickets. Try again.')
 
 def insert_ticket(ticket_id: int, ticket_numbers: list, connection: Redis):
     with connection.pipeline() as pipe:
         for number in ticket_numbers:
             pipe.watch(number)
         for number in ticket_numbers:
-            pipe.append(number, ticket_id)
+            pipe.rpush(number, ticket_id)
 
 def draw_ticket_naive(connection: Redis):
-    connection.watch(CURRENT_TICKET_ID)
-    ticket_id = int(connection.get(CURRENT_TICKET_ID))
-    connection.incr(CURRENT_TICKET_ID)
-
     ticket_numbers = sample(VALID_NUMBERS, 5)
     ticket_numbers.sort()
+
+    connection.watch(UPCOMING_TICKET_ID)
+    ticket_id = int(connection.get(UPCOMING_TICKET_ID))
+    connection.incr(UPCOMING_TICKET_ID)
     insert_ticket(ticket_id, ticket_numbers, connection)
     connection.unwatch()
     print(f'Ticket #{ticket_id} numbers: {ticket_numbers}')
 
-    return ticket_id
-
 def draw_ticket_slow(connection: Redis):
     with connection.pipeline() as pipe:
-        pipe.watch(CURRENT_TICKET_ID)
-        pipe.multi()
-        ticket_id = int(connection.get(CURRENT_TICKET_ID))
-        sleep(1)
-        pipe.incr(CURRENT_TICKET_ID)
-
         ticket_numbers = sample(VALID_NUMBERS, 5)
         ticket_numbers.sort()
+
+        pipe.watch(UPCOMING_TICKET_ID)
+        pipe.multi()
+        ticket_id = int(connection.get(UPCOMING_TICKET_ID))
+        sleep(1)
+        pipe.incr(UPCOMING_TICKET_ID)
         insert_ticket(ticket_id, ticket_numbers, connection)
+
         try:
             pipe.execute()
             print(f'Ticket #{ticket_id} numbers: {ticket_numbers}')
-            return ticket_id
         except WatchError:
-            pipe.discard()
             print('Another ticket is being updated at the moment. Try drawing again.')
             return None
 
 def main():
     connection = Redis(host='localhost', port=6379, db=0)
-    connection.set(CURRENT_TICKET_ID, 1)
+
+    # Initialize current ticket id
+    if not connection.exists(UPCOMING_TICKET_ID):
+        connection.set(UPCOMING_TICKET_ID, 1)
 
     # Draw tickets in different threads
     draw_threads = [
@@ -84,14 +87,12 @@ def main():
     for thread in draw_threads:
         thread.join()
 
-    upcoming_ticket_id = int(connection.get(CURRENT_TICKET_ID))
+    # Print upcoming ticket id
+    upcoming_ticket_id = int(connection.get(UPCOMING_TICKET_ID))
     print(f'Upcoming ticket #{upcoming_ticket_id}')
 
     # Draw winner tickets
-    winner_numbers = sample(VALID_NUMBERS, 10)
-    winner_numbers.sort()
-    print(f'Winner numbers: {winner_numbers}')
-    find_winner_tickets(winner_numbers, connection)
+    get_winner_tickets(connection)
 
 if __name__ == '__main__':
     main()
