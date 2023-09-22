e3a8b8a4d40efb4472781a917ce2412f1221cc7c
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2023-09-22 20:36:14 +0300

    Add cli for user

--- lab1.py
+++ lab1.py
@@ -7,10 +7,16 @@ from collections import Counter
 UPCOMING_TICKET_ID = 'upcoming_ticket_id'
 
 VALID_NUMBERS = list(range(1, 41))
-NUMBER_OF_WINNER_NUMBERS = 5
 
 def get_winner_tickets(connection: Redis):
-    winner_numbers = sample(VALID_NUMBERS, NUMBER_OF_WINNER_NUMBERS)
+    while True:
+        try:
+            number_of_winner_numbers = int(input('Enter how many numbers to roll: '))
+            break
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+    winner_numbers = sample(VALID_NUMBERS, number_of_winner_numbers)
     winner_numbers.sort()
     print(f'Winner numbers: {winner_numbers}')
 
@@ -63,16 +69,8 @@ def draw_ticket_slow(connection: Redis):
             print(f'Ticket #{ticket_id} numbers: {ticket_numbers}')
         except WatchError:
             print('Another ticket is being updated at the moment. Try drawing again.')
-            return None
-
-def main():
-    connection = Redis(host='localhost', port=6379, db=0)
-
-    # Initialize current ticket id
-    if not connection.exists(UPCOMING_TICKET_ID):
-        connection.set(UPCOMING_TICKET_ID, 1)
-
-    # Draw tickets in different threads
+        
+def draw_three_tickets(connection: Redis):
     draw_threads = [
         Thread(target=draw_ticket_naive, args=(connection,)),
         Thread(target=draw_ticket_slow, args=(connection,)),
@@ -87,12 +85,51 @@ def main():
     for thread in draw_threads:
         thread.join()
 
-    # Print upcoming ticket id
+def upcoming_ticket_id(connection: Redis):
     upcoming_ticket_id = int(connection.get(UPCOMING_TICKET_ID))
     print(f'Upcoming ticket #{upcoming_ticket_id}')
+        
+def print_help():
+    print('''
+    ____________________________________________________________
+          
+    slow        - Draw a ticket using a slow method
+    naive       - Draw a ticket using a naive method
+    three       - Draw three tickets in three threads
+    next        - Show the upcoming ticket id
+    winners     - Start the lottery and get the winner tickets
+    help        - Show this help
+    exit        - Exit the program
+    ____________________________________________________________
+          
+    ''')
 
-    # Draw winner tickets
-    get_winner_tickets(connection)
+processes = {
+    'slow': lambda connection: draw_ticket_slow(connection),
+    'naive': lambda connection: draw_ticket_naive(connection),
+    'three': lambda connection: draw_three_tickets(connection),
+    'next': lambda connection: upcoming_ticket_id(connection),
+    'winners': lambda connection: get_winner_tickets(connection),
+    'help': lambda _: print_help(),
+    'exit': lambda _: exit()
+}
+
+def main():
+    connection = Redis(host='localhost', port=6379, db=0)
+
+    # Initialize current ticket id
+    if not connection.exists(UPCOMING_TICKET_ID):
+        connection.set(UPCOMING_TICKET_ID, 1)
+
+    print('\nWelcome to the lottery!')
+    print_help()
+
+    while True:
+        try:
+            command = input('Enter a command: ')
+            processes[command](connection)
+        except KeyError:
+            print('Invalid command. Enter "help" to see available commands.')
 
 if __name__ == '__main__':
     main()
